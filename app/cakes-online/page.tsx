7483e21d89e0e9642c1a11002d0839bfcc97b543
walter834 <wjave@hsperu.pe>
2025-08-04 17:46:13 -0500

    Update navbar

--- app/cakes-online/page.tsx
+++ app/cakes-online/page.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import React from "react";
 
@@ -10,13 +11,32 @@ export default function CakesOnline() {
       </div>
       <div className="flex justify-between">
         <p>Mostrando 8 de 9 resultados</p>
-        <div className="flex items-center p-2 gap-2 border border-black/20">
-          
-          <input type="text" placeholder="Buscar productos" className="border-none outline-none bg-transparent"/>
-          <Search  className="text-black/30 size-5" />
-          
+        
+          <div className="flex w-full max-w-sm items-center border border-gray-300 rounded-lg px-2.5 py-1.5">
+            <svg
+            
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+               className="h-4 w-4 mr-2.5"
+            >
+              <circle cx="11" cy="11" r="8" />
+              <path d="m21 21-4.3-4.3" />
+            </svg>
+            <input
+              type="search"
+              placeholder="Search..."
+              className="w-full border-0 outline-none"
+            />
+          </div>
         </div>
-      </div>
+     
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Poppins} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,7 +39,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased overflow-x-hidden min-h-screen flex flex-col`}
       >
-        <Header />
+        <Navbar/>
+       
+      <Header/>
+        
         <main className="flex-1">
           {children}
         </main>
