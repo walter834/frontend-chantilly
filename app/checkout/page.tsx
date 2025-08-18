cdca009089ca11f7c687379d0deb7a000e96aca3
Ecarrillo-003 <ecarrillo@hsperu.pe>
2025-08-18 14:07:50 -0500

    add : se creo formulario para procesar pedido (solo vista)

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,24 +1,180 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { FaMagnifyingGlass, FaPencil } from "react-icons/fa6";
+import Header from "./components/header";
 
 export default function Contact() {
     return (
-        <header className="bg-[#c41c1a]">
-            <div className="overflow-x-auto flex justify-start gap-10 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 py-4 border-b border-gray-200 bg-[#c41c1a] shadow-lg">
-                <div className="flex items-center px-2 py-1 gap-3">
-                    <Image
-                        src="/logo.png"
-                        alt="logo"
-                        width={350}
-                        height={350}
-                    />
+        <>
+            <Header />
+            <main>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 pr-24 pl-24 pb-24'>
+                    <div className='pl-5 pr-5 pt-5 bg-white'>
+                        <h1 className='text-[25px] font-bold text-black mb-5'>Confirma y paga tu compra
+                        </h1>
+                        <div className='flex justify-center mb-2 bg-[#c41c1a] p-2'>
+                            <h2 className='text-[18px] font-bold text-white'>Datos del Cliente</h2>
+                        </div>
+                        <div className='border-b-2 border-[#c41c1a] pt-3'>
+                            <h2 className='text-[18px] font-bold text-[#c41c1a]'>DATOS PERSONALES</h2>
+                        </div>
+                        <div className='pb-5 pt-2 font-bold text-[15px]'>
+                            Solicitamos unicamente la información esencial para la finalización de la compra
+                        </div>
+                        <div className='grid grid-cols-2 gap-2'>
+                            <div className='grid grid-cols-1'>
+                                <label htmlFor="name">Nombres (obligatorio)</label>
+                                <input className='border-2 border-[#c41c1a]' type="text" name="name" id="name" placeholder="Nombres" />
+                            </div>
+                            <div className='grid grid-cols-1'>
+                                <label htmlFor="lastName">Apellidos (obligatorio)</label>
+                                <input className='border-2 border-[#c41c1a]' type="text" name="lastName" id="lastName" placeholder="Apellidos" />
+                            </div>
+                            <div className='grid grid-cols-1'>
+                                <label htmlFor="address">Dirección</label>
+                                <input className='border-2 border-[#c41c1a]' type="text" name="address" id="address" placeholder="Dirección" />
+                            </div>
+                            <div className='grid grid-cols-1'>
+                                <label htmlFor="phone">Celular (obligatorio)</label>
+                                <input className='border-2 border-[#c41c1a]' type="tel" name="phone" id="phone" placeholder="Celular" />
+                            </div>
+                            <div className='grid grid-cols-1'>
+                                <label htmlFor="document">Documento de identidad</label>
+                                <select name="document" id="document" className='border-2 border-[#c41c1a]'>
+                                    <option value="CC">DOCUMENTO NACIONAL DE IDENTIDAD</option>
+                                    <option value="CE">CARNET DE EXTRANJERIA</option>
+                                    <option value="TI">PASAPORTE</option>
+                                    <option value="TI">CARNET PTP</option>
+                                </select>
+                            </div>
+                            <div className='grid grid-cols-1'>
+                                <label htmlFor="documentNumber">Numero de documento de identidad</label>
+                                <input className='border-2 border-[#c41c1a]' type="text" name="documentNumber" id="documentNumber" placeholder="Numero de documento de identidad" />
+                            </div>
+                        </div>
+                        <div className='flex justify-center mt-4'>
+                            <button className='bg-[#c41c1a] text-white py-2 px-4 rounded cursor-pointer'>Confirmar tus datos</button>
+                        </div>
+                    </div>
+                    <div className='p-6 bg-white'>
+                        <div className='flex justify-between'>
+                            <h1 className='text-[25px] font-bold text-black mb-2'>Resumen de la compra</h1>
+                            <a href="#" className='flex items-center gap-2 text-[#c41c1a] hover:text-[#c41c1a]'> <FaPencil /> Editar carrito </a>
+                        </div>
+                        <div>
+                            <table className='w-full'>
+                                <thead className='bg-[#c41c1a] text-white h-12'>
+                                    <tr>
+                                        <th>Imagen</th>
+                                        <th>Producto</th>
+                                        <th>Cantidad</th>
+                                        <th>Precio S/</th>
+                                        <th>Total S/</th>
+                                    </tr>
+                                </thead>
+                                <tbody className='bg-[#f5f5f5] text-center border-b-2 border-[#c41c1a]'>
+                                    <tr>
+                                        <td><Image src="/logo.png" alt="logo" width={50} height={50} /></td>
+                                        <td className='text-left max-w-[200px]'>TORTA BAUTIZO NIÑO CELESTE CON NUBES
+                                            <ul className='list-none text-gray-500'>
+                                                <li>Porciones: 35 a 40 porciones</li>
+                                                <li>Medidas: 32cm x 24cm</li>
+                                                <li>Keke: Keke de Novia</li>
+                                                <li>Relleno: Manjar de Casa</li>
+                                                <li>Recojo: 2025-08-20</li>
+                                                <li>Dedicatoria: 1</li>
+                                            </ul>
+                                        </td>
+                                        <td>1</td>
+                                        <td>100.00</td>
+                                        <td>100.00</td>
+                                    </tr>
+                                </tbody>
+                                <tfoot className='text-[#c41c1a]'>
+                                    <tr className='text-[20px]'>
+                                        <td colSpan={4} className="text-left font-bold pr-4">TOTAL</td>
+                                        <td className="text-right font-bold">S/ 100.00</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                            <div className='grid grid-cols-1 gap-2 pt-3'>
+                                <div className='flex items-center gap-2'>
+                                    <input type="checkbox" name="terms" id="terms" />
+                                    <label htmlFor="terms" className='text-[18px]'>He leído y acepto las condiciones de <Link target="_blank" href="/politicas-privacidad" className='text-[#0C37ED]'>tratamiento de datos personales</Link></label>
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <input type="checkbox" name="conditions" id="conditions" />
+                                    <label htmlFor="conditions" className='text-[18px]'>He leído y acepta nuestros <Link target="_blank" href="/terminos-condiciones" className='text-[#0C37ED]'>términos y condiciones</Link></label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex justify-center mt-4'>
+                            <button className='bg-[#c41c1a] text-white py-2 w-full rounded cursor-pointer'>PAGAR</button>
+                        </div>
+                    </div>
+                    <div className='pl-5 pr-5 p-5 bg-white'>
+                        <div className='flex justify-center mb-2 bg-[#c41c1a] p-2'>
+                            <h2 className='text-[18px] font-bold text-white'>Datos de facturación</h2>
+                        </div>
+                        <div className='border-b-2 border-[#c41c1a] pt-3'>
+                            <h2 className='text-[18px] font-bold text-[#c41c1a]'>DOCUMENTO DE VENTA                            </h2>
+                        </div>
+                        <div className='flex justify-around items-center gap-5'>
+                            <div className='pb-5 pt-2 text-[15px]'>
+                                <input type="radio" name="receipt" value="receipt" id='receipt' />
+                                <label className="ml-2" htmlFor="receipt">BOLETA DE VENTA</label>
+                            </div>
+                            <div className='pb-5 pt-2 text-[15px]'>
+                                <input type="radio" name="receipt" value="invoice" id='invoice' />
+                                <label className="ml-2" htmlFor="invoice">FACTURA</label>
+                            </div>
+                        </div>
+                        <div className='border-b-2 border-[#c41c1a] pb-3'>
+                            <h2 className='text-[18px] font-bold text-black'>Datos de la Empresa</h2>
+                        </div>
+                        <div className='grid grid-cols-1 gap-2 pt-3'>
+                            <div className="grid grid-cols-1">
+                                <label htmlFor="ruc">RUC</label>
+                                <div className="grid grid-cols-[1fr_auto] gap-2">
+                                    <input className="border-2 border-[#c41c1a]" type="text" name="ruc" id="ruc" placeholder="RUC" />
+                                    <button className="bg-[#c41c1a] text-white py-2 px-4 rounded cursor-pointer">
+                                        <FaMagnifyingGlass />
+                                    </button>
+                                </div>
+                            </div>
+                            <div className='grid grid-cols-1'>
+                                <label htmlFor="razonSocial">Razón Social</label>
+                                <input className='border-2 border-[#c41c1a]' type="text" name="razonSocial" id="razonSocial" placeholder="Razón Social" />
+                            </div>
+                            <div className='grid grid-cols-1'>
+                                <label htmlFor="address">Domicilio Fiscal</label>
+                                <input className='border-2 border-[#c41c1a]' type="text" name="address" id="address" placeholder="Domicilio Fiscal" />
+                            </div>
+                        </div>
+                        <div className='pt-5'>
+                            <div className='flex justify-center mb-2 bg-[#c41c1a] p-2'>
+                                <h2 className='text-[18px] font-bold text-white'>Datos para la entrega</h2>
+                            </div>
+                            <div className='border-b-2 border-[#c41c1a] pt-3'>
+                                <h2 className='text-[18px] font-bold text-[#c41c1a]'>TIPO DE ENTREGA                            </h2>
+                            </div>
+                            <div className='flex justify-around items-center gap-5'>
+                                <div className='pb-5 pt-2 text-[15px]'>
+                                    <input type="radio" name="deliveryType" value="deliveryType" id='deliveryType' defaultChecked />
+                                    <label className="ml-2" htmlFor="deliveryType">Recoger <b>GRATIS</b> en tienda</label>
+                                </div>
+                            </div>
+                            <div className='border-b-2 border-[#c41c1a] pb-3'>
+                                <h2 className='text-[18px] font-bold text-black'>Elige el local más cercano a ti:</h2>
+                            </div>
+                            <div className='grid grid-cols-1 gap-2 pt-3'>
 
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex items-end px-2 py-1 gap-3">
-                    <h1 className="text-white text-[20px] font-semibold">
-                        ¿Necesitas ayuda? Escríbenos al 955 122 100
-                    </h1>
-                </div>
-            </div>
-        </header>
+            </main>
+        </>
     );
+
 }
---
+++ app/terminos-condiciones/page.tsx
@@ -0,0 +1,65 @@
+import Header from "../checkout/components/header";
+
+export default function Terminos() {
+    return (
+        <>
+            <Header />
+            <div className="grid grid-cols-1 lg:grid-cols-1 gap-6 pr-24 pl-24 pb-24">
+                <h1 className="text-[35px] font-bold text-gray-800 mb-2 text-center">Términos y Condiciones</h1>
+                <h2 className="text-[20px] font-bold text-black">1. Introducción</h2>
+
+                <p>Bienvenido a COMERCIALIZADORA AR LA CASA DEL CHANTILLY S.A.C. identificada con RUC N° 20552150148, desde ahora LA CASA DEL CHANTILLY, con domicilio fiscal en LOTE. 176 INT. 2B FND. CHACRA CERRO (A 3 CDRAS PARAD ACUARIO AV UNIVERSITARIA) LIMA - LIMA - COMAS. Al utilizar nuestro sitio web, aceptas cumplir y estar sujeto a los siguientes términos y condiciones. Por favor, léelos detenidamente antes de realizar cualquier compra.</p>
+
+                <h2 className="text-[20px] font-bold text-black">2. Definiciones</h2>
+
+                <p>Cliente: Persona que realiza una compra en nuestro sitio web.
+Tienda: LA CASA DEL CHANTILLY, el sitio web donde se realizan las compras.
+Productos: Bienes ofrecidos para la venta en nuestro sitio web.</p>
+
+                <h2 className="text-[20px] font-bold text-black">3. Uso del Sitio Web</h2>
+
+                <p>Al acceder a nuestro sitio web, garantizas que tienes al menos 18 años o que utilizas el sitio bajo la supervisión de un adulto. Te comprometes a proporcionar información veraz y actualizada en todo momento.</p>
+
+                <h2 className="text-[20px] font-bold text-black">4. Proceso de Compra</h2>
+
+                <p>Selección de Productos: Navega por nuestro catálogo y selecciona los productos que deseas comprar.
+Carrito de Compras: Revisa tu selección en el carrito de compras y procede al pago.
+Pago: Aceptamos varios métodos de pago, incluyendo tarjetas de crédito, débito y billeteras digitales.
+Confirmación: Recibirás un correo electrónico de confirmación una vez que tu pedido haya sido procesado.</p>
+
+                <h2 className="text-[20px] font-bold text-black">5. Precios y Disponibilidad</h2>
+
+                <p>Todos los precios están en SOLES e incluyen impuestos aplicables. Nos reservamos el derecho de modificar los precios en cualquier momento. La disponibilidad de los productos está sujeta a cambios sin previo aviso.</p>
+
+                <h2 className="text-[20px] font-bold text-black">6. Envío y Entrega</h2>
+
+                <p>Plazos de Entrega: Los tiempos de entrega varían según la ubicación y el método de envío seleccionado.
+Costos de Envío: Los costos de envío se calcularán al finalizar la compra.
+Seguimiento: Proporcionaremos un número de seguimiento para que puedas rastrear tu pedido.</p>
+
+                <h2 className="text-[20px] font-bold text-black">7. Devoluciones y Reembolsos</h2>
+
+                <p>Política de Devoluciones: Sólo se aceptan cambios y/o devoluciones dentro de los 30 primeros minutos después de realizada la compra siempre y cuando el producto no haya sido retirado de tienda. Solo se aceptan cambios y/o devoluciones de un producto que haya sido retirado de tienda dentro de las 03 primeras horas después de realizada la compra con el sustento correspondiente.</p>
+
+                <p>Condiciones: Los productos deben estar en su estado original y sin usar.
+Reembolsos: Los reembolsos se procesarán a través del método de pago original.</p>
+
+                <h2 className="text-[20px] font-bold text-black">8. Privacidad</h2>
+
+                <p>Nos comprometemos a proteger tu privacidad. Consulta nuestra Política de Privacidad para obtener más información sobre cómo recopilamos y utilizamos tus datos personales.</p>
+
+                <h2 className="text-[20px] font-bold text-black">9. Propiedad Intelectual</h2>
+
+                <p>Todo el contenido de este sitio web, incluyendo textos, gráficos, logotipos, imágenes y software, es propiedad de COMERCIALIZADORA AR LA CASA DEL CHANTILLY S.A.C. y está protegido por las leyes de propiedad intelectual.</p>
+
+                <h2 className="text-[20px] font-bold text-black">10. Modificaciones de los Términos</h2>
+
+                <p>Nos reservamos el derecho de modificar estos términos y condiciones en cualquier momento. Las modificaciones entrarán en vigor inmediatamente después de su publicación en el sitio web.</p>
+
+                <h2 className="text-[20px] font-bold text-black">11. Contacto</h2>
+
+                <p>Si tienes alguna pregunta o inquietud, no dudes en contactarnos a través de comercializadora.chantilly@gmail.com.</p>
+            </div>
+        </>
+    );
+}
